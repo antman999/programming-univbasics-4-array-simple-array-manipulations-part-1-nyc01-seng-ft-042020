0e5588a221d598cc35975546c3dbede4dba02bae
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-03-30 18:10:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,8 @@ def using_push(colors_in_the_rainbow,next_color)
  next_color = "violet"
  colors_in_the_rainbow.push(next_color)
 end
+
+def using_unshift(array,string)
+  array.unshift(string)
+  
+end
